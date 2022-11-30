08c4338d7c45b5eec53e43679b3d3846cc5179b0
Samuel Soares Guimarães <samuel.soares@unidesk.com.br>
2022-11-30 11:17:04 -0300

    Aba task and improved

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,25 @@
 import styles from './style.module.scss';
-import Image from 'next/image';
 import Link from 'next/link';
 import SignInButton from '../SignInButton';
 
 export default function Header() {
     return (
         <header className={styles.headerContainer}>
-            <div className={styles.divContainer}>
-                <Link href={"/"}>
-                    <Image src="/images/logo.svg" alt="logo meu board" width={50} height={50} />
+            <div className={styles.headerContent}>
+                <Link href="/">
+                    <img src="/images/logo.svg" alt="Logo Meu board" />
                 </Link>
                 <nav>
-                    <Link href={"/"} legacyBehavior>
+                    <Link href="/" legacyBehavior>
                         <a>Home</a>
                     </Link>
-                    <Link href={"/board"} legacyBehavior>
-                        <a>Meu Board</a>
+                    <Link href="/board" legacyBehavior>
+                        <a>Meu board</a>
                     </Link>
                 </nav>
+
                 <SignInButton />
+
             </div>
         </header>
     )
